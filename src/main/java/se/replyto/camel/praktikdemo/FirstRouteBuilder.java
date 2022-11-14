cd8f5e23ed0d156fdd8004551a73ccb516d55304
Yuriks1 <juris.adijans@iths.se>
2022-11-14 16:45:08 +0100

    Add static final

--- src/main/java/se/replyto/camel/praktikdemo/FirstRouteBuilder.java
+++ src/main/java/se/replyto/camel/praktikdemo/FirstRouteBuilder.java
@@ -1,24 +1,38 @@
 package se.replyto.camel.praktikdemo;
-
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class FirstRouteBuilder extends org.apache.camel.builder.RouteBuilder {
+    private static final String SOURCE_FOLDER = "C:\\Users\\juris\\lia";
+    private static final String DESTINATION_FOLDER = "C:\\Users\\juris\\lia\\out";
+    private static final long DURATION_MILLIS = 10000;
+
+
 
     @Override
     public void configure() throws Exception {
         CamelContext context = new DefaultCamelContext();
+
         context.addRoutes(new FirstRouteBuilder() {
-            public void configure() {
-                from("file:C:\\Users\\juris\\lia?noop=true")
-                        .to("file:C:\\Users\\juris\\lia\\out");
+
+            public void configure() throws Exception {
+
+                from("file:" + SOURCE_FOLDER + "?noop=true").convertBodyTo(String.class).process(exchange -> {
+
+                    System.out.println("Headers :"+ exchange.getMessage().getHeaders());
+                    System.out.println(exchange.getIn().getBody());
+                    exchange.getMessage().setHeader("CamelFileName",exchange.getMessage().getHeader("CamelFileName") + ".out");
+                })
+
+                .to("file:"+ DESTINATION_FOLDER + "?fileName=${header.CamelFileName}");
             }
         });
 
         context.start();
-        Thread.sleep(10000);
+        Thread.sleep(DURATION_MILLIS);
 
         context.stop();
     }
